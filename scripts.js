41d1b89d591795f386fc906c4d41753760b5c58a
Will Nicholson <william.nicholson.dev@gmail.com>
2020-12-03 20:26:47 -0600

    minor refactor - const

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 window.addEventListener("load", function(){
     // TODO: register the handler
-    let searchButton = document.getElementById("search");
+    const searchButton = document.getElementById("search");
     searchButton.addEventListener("click", function(){
-    let input = document.getElementById("q");
+    const input = document.getElementById("q");
     console.log(input.value);
-    let engine = document.querySelector("input[name=engine]:checked");
+    const engine = document.querySelector("input[name=engine]:checked");
     console.log(engine.value);
 
-    let actions = {
+    const actions = {
         "Google": "http://www.google.com/search",
         "DuckDuckGo": "https://www.duckduckgo.com/",
         "Bing": "https://www.bing.com/",
         "Lycos": "https://search.lycos.com/web/"
     }
-    let searchForm = document.getElementById("searchForm");
+    const searchForm = document.getElementById("searchForm");
     searchForm.setAttribute("action", actions[engine.value]);
     });
 });
